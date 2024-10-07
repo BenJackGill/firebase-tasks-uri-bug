3e126a189ea88bb0ab46b7860d6a8530ee31e340
BenJackGill <2043257+BenJackGill@users.noreply.github.com>
2024-10-07 10:51:40 +0700

    one file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,36 @@
 import * as admin from "firebase-admin";
-import { testOnRequest } from "./test-http";
-import { testOnTaskDispatched } from "./test-task";
+import { onRequest } from "firebase-functions/v2/https";
+import { getFunctions } from "firebase-admin/functions";
+import { onTaskDispatched } from "firebase-functions/v2/tasks";
+import * as logger from "firebase-functions/logger";
 
 // Initialize the Firebase app
 admin.initializeApp();
 
-// Export the functions after initialization
-export { testOnRequest, testOnTaskDispatched };
+// The task function
+export const testOnRequest = onRequest(async (request, response) => {
+  const taskPayload = {
+    foo: "bar",
+  };
+  const targetUri = "testOnTaskDispatched"; // <-- This works :)
+  // const targetUri = "locations/us-central1/functions/testOnTaskDispatched"; // <-- This does NOT work :(
+  // const targetUri =
+  //  "projects/demo-project/locations/us-central1/functions/testOnTaskDispatched"; // <-- This does NOT work :(
+  const queue = getFunctions().taskQueue(targetUri);
+
+  try {
+    await queue.enqueue(taskPayload);
+  } catch (error) {
+    console.error("Error scheduling task", error);
+    response.status(500).send("Error scheduling task");
+    return;
+  }
+  response.send("Hello from HTTP ON REQUEST!");
+});
+
+// The http functions
+export const testOnTaskDispatched = onTaskDispatched((request) => {
+  logger.info("Hello logs from TASKS ON TASK DISPATCHED!", {
+    foo: request.data,
+  });
+});
--- functions/src/test-task.ts
+++
@@ -1,8 +0,0 @@
-import { onTaskDispatched } from "firebase-functions/v2/tasks";
-import * as logger from "firebase-functions/logger";
-
-export const testOnTaskDispatched = onTaskDispatched((request) => {
-  logger.info("Hello logs from TASKS ON TASK DISPATCHED!", {
-    foo: request.data,
-  });
-});
